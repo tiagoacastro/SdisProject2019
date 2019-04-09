19cee9f91941817a32fdceebdbc3f3c484c96662
Tiago Castro <tiagoaraujocastro@gmail.com>
2019-04-09 15:49:26 +0100

    started shutdown hook, USE DOOR BUTTON TO TRIGGER

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,7 +1,27 @@
 package code;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Main {
+    private static Peer peer;
+
+    static class Hook extends Thread {
+        @Override
+        public void run() {
+            System.out.println("hey");
+            /*try {
+                ObjectOutputStream out = new ObjectOutputStream(System.out);
+                out.writeObject(peer);
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }*/
+        }
+    }
+
     public static void main(String[] args) {
-        new Peer(args);
+        Runtime.getRuntime().addShutdownHook(new Hook());
+        peer = new Peer(args);
     }
 }
