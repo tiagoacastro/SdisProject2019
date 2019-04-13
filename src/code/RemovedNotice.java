f59a9448566dcc23a4f71ca1c7c02eb8140cb7c6
Tiago Castro <tiagoaraujocastro@gmail.com>
2019-04-13 19:17:41 +0100

    fixes

--- src/code/RemovedNotice.java
+++ src/code/RemovedNotice.java
@@ -8,6 +8,7 @@ import java.io.File;
 public class RemovedNotice implements Runnable {
     private String fileId;
     private int chunkNo;
+    private boolean delete = true;
 
     RemovedNotice(String fileId, int chunkNo) {
         this.fileId = fileId;
@@ -17,33 +18,40 @@ public class RemovedNotice implements Runnable {
 
         if(dir.exists() && dir.isDirectory()){
             File chunk = new File("peer" + Peer.senderId + "/backup/" + fileId + "/chk" + chunkNo);
-            if(!chunk.delete()){
-                System.out.println("Error deleting file " + fileId + " #" + chunkNo);
-                System.exit(-1);
-            }
-
-            String[] list = dir.list();
-
-            if(list != null && list.length == 0){
-                if(!dir.delete()){
-                    System.out.println("Error deleting dir " + fileId);
+            if(chunk.exists()){
+                if(!chunk.delete()){
+                    System.out.println("Error deleting file " + fileId + " #" + chunkNo);
                     System.exit(-1);
                 }
 
-                System.out.println("Deleted dir " + fileId);
+                String[] list = dir.list();
+
+                if(list != null && list.length == 0){
+                    if(!dir.delete()){
+                        System.out.println("Error deleting dir " + fileId);
+                        System.exit(-1);
+                    }
+
+                    System.out.println("Deleted dir " + fileId);
+                }
+
+                System.out.println("Deleted file " + fileId + " #" + chunkNo + " - Used Space: " + Peer.getUsedSpace());
+
+                Key key = new Key(fileId, chunkNo);
+                Peer.stores.get(key).decrement();
             }
-
-            System.out.println("Deleted file " + fileId + " #" + chunkNo + " - Used Space: " + Peer.getUsedSpace());
-
-            Key key = new Key(fileId, chunkNo);
-            Peer.stores.get(key).decrement();
-        }
+            else
+                delete = false;
+        } else
+            delete = false;
     }
 
     @Override
     public void run() {
-        String[] params = new String[]{this.fileId, Integer.toString(this.chunkNo)};
-        String message = Auxiliary.addHeader("REMOVED", params);
-        Channel.sendPacketBytes(Mc.socket, message.getBytes(), Mc.address, Mc.port);
+        if(delete) {
+            String[] params = new String[]{this.fileId, Integer.toString(this.chunkNo)};
+            String message = Auxiliary.addHeader("REMOVED", params);
+            Channel.sendPacketBytes(Mc.socket, message.getBytes(), Mc.address, Mc.port);
+        }
     }
 }
