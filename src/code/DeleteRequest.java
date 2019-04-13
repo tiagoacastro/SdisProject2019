0faa9e02cb1b095ef023bba921fbc3603c6c99cd
Tiago Castro <tiagoaraujocastro@gmail.com>
2019-04-13 19:37:34 +0100

    fixes, STORE, DELETE and RECLAIM all working top kek, must check RECLAIM

--- src/code/DeleteRequest.java
+++ src/code/DeleteRequest.java
@@ -4,6 +4,7 @@ import channels.Channel;
 import channels.Mc;
 
 import java.io.File;
+import java.util.Map;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -19,6 +20,12 @@ public class DeleteRequest implements Runnable {
 
     @Override
     public void run() {
+        for (Map.Entry<Key, Value> entry : Peer.stores.entrySet()) {
+            Key k = entry.getKey();
+
+            if (k.file.equals(fileId))
+                entry.getValue().stores = 0;
+        }
         String[] params = new String[]{this.fileId};
         String message = Auxiliary.addHeader("DELETE", params);
         Channel.sendPacketBytes(Mc.socket, message.getBytes(), Mc.address, Mc.port);
